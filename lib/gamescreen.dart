a5eb795a9b6d4a08b98bf4b4e3772d8190ce3647
Yagizhan Kerim Deger <yagizhan.deger@d-teknoloji.com.tr>
2021-12-28 23:08:31 +0300

    direction up is removed

--- lib/gamescreen.dart
+++ lib/gamescreen.dart
@@ -1,77 +1,99 @@
+//@dart=2.9
 import 'package:flutter/material.dart';
-import 'package:adobe_xd/pinned.dart';
-import './zorluk.dart';
-import 'package:adobe_xd/page_link.dart';
+import 'package:swipe_cards/swipe_cards.dart';
 
-class gamescreen extends StatelessWidget {
+class Content {
+  final String text;
+  final Color color;
+
+  Content({this.text, this.color});
+}
+
+class gamescreen extends StatefulWidget {
   gamescreen({
-    Key? key,
+    Key key,
   }) : super(key: key);
+
+  @override
+  _gamescreen createState() => _gamescreen();
+}
+class _gamescreen extends State<gamescreen> {
+  List<SwipeItem> _swipeItems = List<SwipeItem>();
+  MatchEngine _matchEngine;
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final List<String> _names = ["Red", "Blue", "Green", "Yellow", "Ahhmet"];
+  final List<Color> _colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.orange
+  ];
+
+  @override
+  void initState() {
+    for (int i = 0; i < _names.length; i++) {
+      _swipeItems.add(SwipeItem(
+          content: Content(text: _names[i], color: _colors[i]),
+          likeAction: () {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Liked ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          },
+          nopeAction: () {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Nope ${_names[i]}"),
+              duration: Duration(milliseconds: 500),
+            ));
+          }
+          ));
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff373737),
-      body: Stack(
-        children: <Widget>[
-          Pinned.fromPins(
-            Pin(start: 53.0, end: 26.0),
-            Pin(size: 48.0, middle: 0.438),
-            child: Text(
-              'Oyun ekranı burada olacak..... Şuan tasarım aşamasındadı…..',
-              style: TextStyle(
-                fontFamily: 'Rockwell',
-                fontSize: 20,
-                color: const Color(0xff08f300),
-                fontStyle: FontStyle.italic,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Pinned.fromPins(
-            Pin(size: 61.0, start: 0.0),
-            Pin(size: 63.0, middle: 0.2577),
-            child: PageLink(
-              links: [
-                PageLinkInfo(
-                  transition: LinkTransition.SlideLeft,
-                  ease: Curves.easeOut,
-                  duration: 0.3,
-                  pageBuilder: () => zorluk(),
-                ),
-              ],
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius:
-                      BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
-                  color: const Color(0xff540000),
-                  border:
-                      Border.all(width: 1.0, color: const Color(0xff707070)),
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("YKD"),
+        ),
+        body: Container(
+            child: Column(children: [
+              Container(
+                height: 400,
+                child: Column(
+                  children: <Widget>[
+                    Image.asset("assets/images/ch.jpg"),
+                    Text('cat')
+                  ],
                 ),
               ),
-            ),
-          ),
-          Pinned.fromPins(
-            Pin(start: 0.0, end: 0.0),
-            Pin(size: 208.0, start: 0.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(29.0),
-                  bottomLeft: Radius.circular(29.0),
-                ),
-                color: const Color(0xff1b471f),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0x29000000),
-                    offset: Offset(0, 3),
-                    blurRadius: 6,
+          Container(
+            height: 100,
+            child: SwipeCards(
+              matchEngine: _matchEngine,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: _swipeItems[index].content.color,
+                  child: Text(
+                    _swipeItems[index].content.text,
+                    style: TextStyle(fontSize: 100),
                   ),
-                ],
-              ),
+                );
+              },
+              onStackFinished: () {
+                _scaffoldKey.currentState.showSnackBar(SnackBar(
+                  content: Text("Stack Finished"),
+                  duration: Duration(milliseconds: 500),
+                ));
+              },
             ),
           ),
-        ],
-      ),
-    );
+        ])));
   }
 }
